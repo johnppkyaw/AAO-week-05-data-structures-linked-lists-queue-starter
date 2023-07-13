d4a9567d6906d00a965397b20f2415ae6ee18a61
John <jjpyae@gmail.com>
2023-07-13 12:19:16 -0700

    completes phase 3

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,18 +5,43 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        const newNode = new SinglyLinkedNode(val);
+        if(this.tail) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        } else {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        return ++this.length;
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1);
     }
 
     dequeue() {
+        if (this.length > 0) {
+            this.length--;
+            const dequeued = this.head.value;
+            if (this.length === 0) {
+                this.head = null;
+                this.tail = null;
+                return dequeued;
+            }
+            this.head = this.head.next;
+            return dequeued;
+        } else {
+            return null;
+        }
         // Remove node from front of queue (linked list)
-        
+
         // Write your hypothesis on the time complexity of this method here
+        // O(1);
     }
 
 }
